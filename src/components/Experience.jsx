fd92b0ee985860b7d77ae3c1f970e1e4994f5bc3
MRhacksalive <adityakroy123@gmail.com>
2026-03-14 01:08:40 +0530

    changes to hero10

--- src/components/Experience.jsx
+++ src/components/Experience.jsx
@@ -12,17 +12,22 @@ return(
 <h3>Zemoso Technologies</h3>
 <p>Software Intern</p>
 <ul>
-<li>Worked with NumPy & Pandas</li>
-<li>Built data visualizations</li>
-<li>Applied ML workflows</li>
+<li>Gained Hands-on experience with Python Libraries such as Numpy and Pandas for data
+manipulation and analysis.</li>
+<li>Learned to visualize complex datasets using Matplotlib and Seaborn, building clear data
+visualization.</li>
+<li>Applied ML workflows using Python libraries for real-world datasets, developing insights
+through visualization and feature extraction</li>
 </ul>
 </div>
 
 <div className="card">
 <h3>Mars Rover Manipal</h3>
 <ul>
-<li>Predictive modelling for sensor systems</li>
-<li>Hardware-software integration</li>
+<li>Conducted data-driven analysis on sensor-actuator systems, emphasizing predictive
+modeling for hardware-software integration.</li>
+<li>Collaborated on system-level integration with sensors and actuators.</li>
+<li>Prepared structured technical reports covering backend API design, database schema organization, and frontend integration.</li>
 </ul>
 </div>
 
