fed3134fc8f31810e2475f818f99a72d31848c66
MRhacksalive <adityakroy123@gmail.com>
2026-03-14 23:32:55 +0530

    final bg

--- src/App.jsx
+++ src/App.jsx
@@ -6,16 +6,38 @@ import Projects from "./components/Projects";
 import Stack from "./components/Stack";
 import Contact from "./components/Contact";
 
+/* PARTICLES */
+
+function Particles(){
+
+const particles = Array.from({length:30});
+
+return(
+<div className="particles">
+
+{particles.map((_,i)=>(
+<div
+key={i}
+className="particle"
+style={{
+left: Math.random()*100+"vw",
+animationDuration: 2 + Math.random()*6 + "s",
+animationDelay: Math.random()*2 + "s"
+}}
+></div>
+))}
+
+</div>
+);
+
+}
+
 function App() {
   return (
     <div className="App">
 
-      {/* Background glow elements */}
-      <div className="glow"></div>
-      <div className="glow"></div>
-      <div className="glow"></div>
-      
-      {/* Website Sections */}
+      <Particles/>
+
       <Navbar />
       <Hero />
       <About />
@@ -23,6 +45,7 @@ function App() {
       <Projects />
       <Stack />
       <Contact />
+
     </div>
   );
 }
